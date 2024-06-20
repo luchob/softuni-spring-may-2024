070b3d51f55f9ed2bf58d38c5f7e63dbccda785e
rd-lachezar-balev <lachezar.balev@gmail.com>
2024-06-20 17:30:27 +0300

    HATEOAS DEMO

---
+++ hateoas/src/main/java/bg/softuni/hateoas/service/StudentService.java
@@ -0,0 +1,56 @@
+package bg.softuni.hateoas.service;
+
+import bg.softuni.hateoas.dto.OrderDTO;
+import bg.softuni.hateoas.dto.StudentDTO;
+import bg.softuni.hateoas.entity.OrderEntity;
+import bg.softuni.hateoas.entity.StudentEntity;
+import bg.softuni.hateoas.repository.StudentRepository;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+import org.springframework.stereotype.Service;
+
+@Service
+public class StudentService {
+
+  private final StudentRepository studentRepository;
+
+  public StudentService(StudentRepository studentRepository) {
+    this.studentRepository = studentRepository;
+  }
+
+  public List<StudentDTO> getAllStudents() {
+    return studentRepository.findAll().stream().map(this::map).toList();
+  }
+
+  public List<OrderDTO> getStudentOrders(Long studentId) {
+    return getStudentById(studentId).
+        map(StudentDTO::getOrders).
+        orElseGet(ArrayList::new);
+  }
+
+  public Optional<StudentDTO> getStudentById(Long studentId) {
+    return studentRepository.findById(studentId).map(this::map);
+  }
+
+  private StudentDTO map(StudentEntity entity) {
+
+    var orders = entity.
+        getOrders().
+        stream().
+        map(this::map).
+        toList();
+
+    return new StudentDTO().
+        setAge(entity.getAge()).
+        setDeleted(entity.isDeleted()).
+        setId(entity.getId()).
+        setName(entity.getName()).
+        setOrders(orders);
+  }
+
+  private OrderDTO map(OrderEntity entity) {
+    return new OrderDTO().setStudentId(entity.getStudent().getId()).
+        setCourseId(entity.getCourse().getId());
+  }
+}
---
+++ hateoas/src/main/java/bg/softuni/hateoas/web/StudentController.java
@@ -0,0 +1,27 @@
+package bg.softuni.hateoas.web;
+
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
+
+import bg.softuni.hateoas.dto.OrderDTO;
+import bg.softuni.hateoas.dto.StudentDTO;
+import bg.softuni.hateoas.service.StudentService;
+import java.util.ArrayList;
+import java.util.List;
+import org.springframework.hateoas.CollectionModel;
+import org.springframework.hateoas.EntityModel;
+import org.springframework.hateoas.Link;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequestMapping("/students")
+@RestController
+public class StudentController {
+
+
+
+}
